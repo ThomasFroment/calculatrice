c1a172be92a87683209d0bfad18a8a0b62c9ca7d
Thomas <thomasfroment2@outlook.fr>
2023-03-16 21:10:23 +0100

    Overhauled the JavaScript and added fallbacks.

--- script.js
+++ script.js
@@ -1,109 +1,138 @@
-var operator = "";
+var currNumber = "";
+var prevNumber = "";
 
-var previousValue = "";
-var currentValue = "";
+var currOperator = "";
+var chainOperation = false;
 
-const mainDisplay = document.querySelector("[data-mainDisplay]");
-const altDisplay = document.querySelector("[data-altDisplay]");
+const INPUT = document.querySelector("[data-input]");
+const OUTPUT = document.querySelector("[data-output]");
 
-function clear() {
-   operator = "";
-   previousValue = "";
-   currentValue = "";
+function reset(err) {
+   currNumber = "";
+   prevNumber = "";
+
+   currOperator = "";
+
+   OUTPUT.innerText = "";
+   if (err) {
+      INPUT.innerText = err;
+      return;
+   }
+   INPUT.innerText = "0";
 }
 
 function calculate() {
    var result;
 
-   const currentFloat = parseFloat(currentValue);
-   const previousFloat = parseFloat(previousValue);
+   const currFloat = parseFloat(currNumber);
+   const prevFloat = parseFloat(prevNumber);
 
-   switch (operator) {
+   switch (currOperator) {
       case "÷":
-         result = previousFloat / currentFloat;
+         if (currFloat === 0) {
+            return NaN;
+         }
+         result = prevFloat / currFloat;
          break;
       case "×":
-         result = previousFloat * currentFloat;
+         result = prevFloat * currFloat;
          break;
       case "−":
-         result = previousFloat - currentFloat;
+         result = prevFloat - currFloat;
          break;
       case "+":
-         result = previousFloat + currentFloat;
+         result = prevFloat + currFloat;
          break;
    }
+
    return result.toString();
 }
 
-function appendValue(text) {
-   if (text == "." && currentValue.includes(".")) {
+function appendDigit(digit) {
+   if (digit === "." && currNumber.includes(".")) {
       return;
    }
-   currentValue += text;
-   mainDisplay.innerText = currentValue;
+   if (digit === "0" && currNumber === "0") {
+      return;
+   }
+   currNumber += digit;
+   INPUT.innerText = currNumber;
 }
 
-function selectOperator(text) {
-   if (currentValue == "") {
-      if (previousValue == "") {
-         previousValue = "0";
+function selectOperator(symbol) {
+   if (currNumber === "" || prevNumber === "") {
+      if (prevNumber === "") {
+         prevNumber = currNumber || "0";
       }
-      operator = text;
-      altDisplay.innerText = `${previousValue} ${operator}`;
+      currOperator = symbol;
+      currNumber = "";
+
+      OUTPUT.innerText = `${prevNumber} ${currOperator}`;
       return;
    }
 
-   if (previousValue != "") {
-      const result = calculate();
-      operator = text;
-      previousValue = result;
-      currentValue = "";
-      altDisplay.innerText = `${previousValue} ${operator}`;
-      return;
-   }
-
-   operator = text;
-   previousValue = currentValue;
-   currentValue = "";
-   altDisplay.innerText = `${previousValue} ${operator}`;
-}
-
-for (let el of document.querySelectorAll("[data-numbers]")) {
-   el.addEventListener("click", (event) => {
-      appendValue(event.target.textContent);
-   });
-}
-
-for (let el of document.querySelectorAll("[data-operators]")) {
-   el.addEventListener("click", (event) => {
-      selectOperator(event.target.textContent);
-
-      mainDisplay.setAttribute("data-placeholder", previousValue);
-      mainDisplay.innerText = currentValue;
-   });
-}
-
-document.querySelector("[data-equals]").addEventListener("click", () => {
-   if (currentValue == "") {
-      currentValue = mainDisplay.getAttribute("data-placeholder");
-   }
    const result = calculate();
+   if (isNaN(result)) {
+      reset("ERR");
+      return;
+   }
+   currOperator = symbol;
 
-   mainDisplay.setAttribute("data-placeholder", result);
-   mainDisplay.innerText = "";
-   altDisplay.innerText = `${previousValue} ${operator} ${currentValue} =`;
-   clear();
+   prevNumber = result;
+   currNumber = "";
+
+   INPUT.innerText = result;
+   OUTPUT.innerText = `${prevNumber} ${currOperator}`;
+}
+
+const NUMBERS = document.querySelectorAll("[data-numbers]");
+NUMBERS.forEach((NUMBER) => {
+   NUMBER.addEventListener("click", function (e) {
+      appendDigit(e.target.textContent);
+   });
 });
 
-document.querySelector("[data-clear]").addEventListener("click", () => {
-   clear();
-   mainDisplay.setAttribute("data-placeholder", "0");
-   mainDisplay.innerText = "";
-   altDisplay.innerText = "";
+const CLEAR = document.querySelector("[data-clear]");
+CLEAR.addEventListener("click", function () {
+   reset();
 });
 
-document.querySelector("[data-delete]").addEventListener("click", () => {
-   currentValue = currentValue.substring(0, currentValue.length-1);
-   mainDisplay.innerText = currentValue;
-   mainDisplay.setAttribute("data-placeholder", "0");
+const DELETE = document.querySelector("[data-delete]");
+DELETE.addEventListener("click", function () {
+   currNumber = currNumber.substring(0, currNumber.length - 1);
+   if (currNumber !== "") {
+      INPUT.innerText = currNumber;
+      return;
+   }
+   INPUT.innerText = "0";
+});
+
+const EQUALS = document.querySelector("[data-equals]");
+EQUALS.addEventListener("click", function () {
+   if (currOperator === "" || prevNumber === "") {
+      return;
+   }
+   if (currNumber === "") {
+      currNumber = INPUT.innerText;
+   }
+
+   const result = calculate();
+   if (isNaN(result)) {
+      reset("ERR");
+      return;
+   }
+   INPUT.innerText = result;
+   OUTPUT.innerText = `${prevNumber} ${currOperator} ${currNumber} =`;
+
+   currOperator = "";
+
+   currNumber = "";
+   prevNumber = result;
+});
+
+const OPERATORS = document.querySelectorAll("[data-operators]");
+OPERATORS.forEach((OPERATOR) => {
+   OPERATOR.addEventListener("click", function (e) {
+      selectOperator(e.target.textContent);
+   });
 });
